55815b50a14e90a1f8a20fe29e9cc08ee47f2fa9
ifty03 <ashikulislamb84@gmail.com>
2022-05-10 17:31:37 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Pages/About/About";
+import Appointment from "./Pages/Appointment/Appointment";
+import Contact from "./Pages/Contact/Contact";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
 import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
@@ -12,6 +16,10 @@ function App() {
         <Route path="/home" element={<Home />}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/appointment" element={<Appointment />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,93 +1,106 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navManue = (
+    <>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-accent text-white" : "bg-base-100"
+          }
+          to="/home"
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-accent text-white" : "bg-base-100"
+          }
+          to="/about"
+        >
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-accent text-white" : "bg-base-100"
+          }
+          to="/appointment"
+        >
+          Appointment
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-accent text-white" : "bg-base-100"
+          }
+          to="/reviews"
+        >
+          Reviews
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-accent text-white" : "bg-base-100"
+          }
+          to="/contact"
+        >
+          Contact Us
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "bg-accent text-white" : "bg-base-100"
+          }
+          to="/login"
+        >
+          Login
+        </NavLink>
+      </li>
+    </>
+  );
   return (
-    <div class="navbar container mx-auto bg-base-00 flex justify-between">
-      <div class="navbar-start ">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li tabindex="0">
-              <a>
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="p-2 bg-base-100">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
-        <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-      </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabindex="0">
-            <a>
-              Parent
+    <div>
+      <div className="navbar container mx-auto bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost lg:hidden">
               <svg
-                class="fill-current"
                 xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
+                className="h-5 w-5"
+                fill="none"
                 viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
               </svg>
-            </a>
-            <ul class="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navManue}
             </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+          </div>
+          <NavLink to="/" className="btn btn-ghost normal-case text-xl">
+            Doctors Portal
+          </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex lg:ml-auto">
+          <ul className="menu menu-horizontal p-0">{navManue}</ul>
+        </div>
       </div>
     </div>
   );
